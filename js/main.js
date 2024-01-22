2ebfc5e484cb19c4633ace3add2ee8303528bf82
nakataitex <nakatahirokiitex@gmail.com>
2024-01-22 13:18:39 +0900

    gainと音量バーを追加、コメント数調整、スクリプト簡略化
    1.ゲイン追加
    2.イベントリスナーとfunctionを分けていたものを1つにまとめた。
    ・ページが読み込まれた時のスクリプト

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
-//鍵盤を1オクターブ分設置するスクリプト
-function keySet() {
+//ページが読み込まれた時に鍵盤を1オクターブ分設置するスクリプト
+window.onload = function () {
     for (let i = 0; i < 7; i++) {
         $('.piano').append(`<div class='key'><button>${scale[i]}</button></div>`);
     };
-};
-
-//ページが読み込まれたときに実行
-window.onload = function () {
-    keySet();
 }
 
-//簡易ピアノ用の基本的な部分の定数宣言
-var actx = new AudioContext();// WebAudioAPIコンテキスト
-let oscillator = null; //オシレータ
-const scaleHz = [261.626, 293.665, 329.628, 349.228, 392.000, 440.000, 493.883];//C4から1オクターブ分の周波数
-const scale = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];//表示用の音階
+//ピアノに盛り込む機能の定数･変数
 const piano_status = $(".sound_view span");//音階を文字として表示する際に使う
 let scaleHideCount = null;//表示された音階を非表示にするタイマー
 
+//基本的な箇所の定数宣言
+var actx = new AudioContext();
+var oscillator = null;
+const scaleHz = [261.626, 293.665, 329.628, 349.228, 392.000, 440.000, 493.883];//C4から1オクターブ分の周波数
+const scale = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];//表示用の音階
+
+//音量管理
+var gainNode = actx.createGain();
+var volume = $("#volume").val();//ボリューム管理用
+
+//音量を変更
+function changeVolume() {
+    var volume = $("#volume").val();
+    $("#volume_view").text(parseInt(volume, 10) + '%');
+    gainNode.gain.value = volume / 100;
+}
+
+//音量バー操作時に実行
+$("#volume").on("change", function () {
+    var volume = $("#volume").val();
+    $("#volume_view").text(parseInt(volume, 10) + '%');
+    gainNode.gain.value = volume / 100;
+});
+
 //鍵盤を押した時の挙動
 $(".piano").on("mousedown", ".key", function () {
-    let i = $(".key").index(this);//キーの場所を
-    oscillator = actx.createOscillator();//オシレーター作成
-    oscillator.type = "sine";//波の種類
+    let i = $(".key").index(this);
+    oscillator = actx.createOscillator();
+    oscillator.type = "sine";
     oscillator.frequency.setValueAtTime(scaleHz[i], actx.currentTime);//押されたキーによって音を変える様に
-    oscillator.connect(actx.destination);//出力先に接続
-    oscillator.start();//開始
-    piano_status.text(scale[i]);//音を文字として表示
+    oscillator.connect(gainNode);
+    gainNode.connect(actx.destination);
+    oscillator.start();
+    piano_status.text(scale[i]);
     clearTimeout(scaleHideCount);//音階を表示中に"文字非表示"が割り込まない様に
 });
 
@@ -42,3 +58,4 @@ $(document).on("mouseup", function () {
         }, 1000);
     }
 });
+
