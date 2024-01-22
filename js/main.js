db0a4c15c13d90e03ca65adeba1f9b3a2d2c2452
nakataitex <nakatahirokiitex@gmail.com>
2024-01-22 11:48:02 +0900

    要件に沿って修正
    ファイル名の変更、鍵盤にdiv追加。
    コメントの編集、CSSのレイアウトの破綻を少し修正。

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-const actx = new AudioContext();// WebAudioAPIコンテキスト
-let oscillator = null; //オシレーターのnull指定
+//鍵盤を1オクターブ分設置するスクリプト
+function keySet() {
+    for (let i = 0; i < 7; i++) {
+        $('.piano').append(`<div class='key'><button>${scale[i]}</button></div>`);
+    };
+};
 
-// 定数宣言
-const scaleHz = [261.626, 293.665, 329.628, 349.228, 392.000, 440.000, 493.883];//音階
+//ページが読み込まれたときに実行
+window.onload = function () {
+    keySet();
+}
+
+//簡易ピアノ用の基本的な部分の定数宣言
+var actx = new AudioContext();// WebAudioAPIコンテキスト
+let oscillator = null; //オシレータ
+const scaleHz = [261.626, 293.665, 329.628, 349.228, 392.000, 440.000, 493.883];//C4から1オクターブ分の周波数
 const scale = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];//表示用の音階
 const piano_status = $(".sound_view span");//音階を文字として表示する際に使う
-const key = $(".key");//キー管理用
 let scaleHideCount = null;//表示された音階を非表示にするタイマー
 
-
-
-/* 
-//音階を挿入(追加機能に鍵盤を追加する機能を実装する場合に使えそう)
-for (let i = 0; i < key.length; i++) {
-    $(key[i]).text(scale[i % scale.length]);
-}
- */
-
 //鍵盤を押した時の挙動
-$(".key").on("mousedown", function () {
+$(".piano").on("mousedown", ".key", function () {
     let i = $(".key").index(this);//キーの場所を
     oscillator = actx.createOscillator();//オシレーター作成
     oscillator.type = "sine";//波の種類
@@ -26,7 +27,7 @@ $(".key").on("mousedown", function () {
     oscillator.connect(actx.destination);//出力先に接続
     oscillator.start();//開始
     piano_status.text(scale[i]);//音を文字として表示
-    clearTimeout(scaleHideCount);//音階を表示中に"なし"が割り込んで表示されない様に
+    clearTimeout(scaleHideCount);//音階を表示中に"文字非表示"が割り込まない様に
 });
 
 //マウスが離された時の挙動
@@ -35,9 +36,9 @@ $(document).on("mouseup", function () {
     if (oscillator) {
         oscillator.stop();
         oscillator = null;
-        //音が消えてから1秒後に「ステータス:なし」と表示されるように変更
+        //音が消えてから1秒後非表示に変更
         scaleHideCount = setTimeout(function () {
-            piano_status.text("なし");
+            piano_status.text("");
         }, 1000);
     }
 });
