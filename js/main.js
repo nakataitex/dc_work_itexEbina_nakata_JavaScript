fe7a13fbc2d87e864e9d14bc033548f084df612a
nakataitex <nakatahirokiitex@gmail.com>
2024-01-23 14:12:37 +0900

    不具合の為、一時的にgain関連削除
    自宅PCでは不具合はなかったが念の為gain関連を削除して他の機能を実装する。

--- js/main.js
+++ js/main.js
@@ -3,32 +3,16 @@ window.onload = function () {
     for (let i = 0; i < 7; i++) {
         $(".piano").append(`<div class='key'><button>${scale[i]}</button></div>`);
     };
-    $("#volume_view").text($("#volume").val() + "%");
 }
 
 //ピアノに盛り込む機能の定数･変数
 const piano_status = $(".sound_view span");//音階を文字として表示する際に使う
 let scaleHideCount = null;//表示された音階を非表示にするタイマー
-
 //基本的な箇所の定数宣言
 var actx = new AudioContext();// WebAudioAPIコンテキスト
 var oscillator = null;
-/* const scaleHz = [261.626, 293.665, 329.628, 349.228, 392.000, 440.000, 493.883];//C4から1オクターブ分の周波数
-小数点以下がある事で処理落ちしてるかもしれないので試しに↓を使用 */
 const scaleHz = [261, 293, 329, 349, 392, 440, 493];//C4から1オクターブ分の周波数
 const scale = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];//表示用の音階
-
-//音量管理
-var gainNode = actx.createGain();
-var volume = $("#volume").val();//ボリューム管理用
-var GainToVolume = (Math.floor(volume * 100) / 100);
-
-// ボリュームを変更
-$("#volume").on("change", function () {
-    var volume = $("#volume").val();
-    $("#volume_view").text(parseInt(volume, 10) + '%');
-    gainNode.gain.value = volume / 100;
-});
 
 //鍵盤を押した時の挙動
 $(".piano").on("mousedown", ".key", function () {
@@ -36,8 +20,7 @@ $(".piano").on("mousedown", ".key", function () {
     oscillator = actx.createOscillator();
     oscillator.type = "sine";
     oscillator.frequency.setValueAtTime(scaleHz[i], actx.currentTime);//押されたキーによって音を変える様に
-    oscillator.connect(gainNode);
-    gainNode.connect(actx.destination);
+    oscillator.connect(actx.destination);
     oscillator.start();
     piano_status.text(scale[i]);
     clearTimeout(scaleHideCount);//音階を表示中に"文字非表示"が割り込まない様に
@@ -54,3 +37,4 @@ $(document).on("mouseup", function () {
         }, 1000);
     }
 });
+
