c29050388c2a645cc36834b861b9efc282d018b7
nakataitex <nakatahirokiitex@gmail.com>
2024-02-07 15:51:17 +0900

    テスト環境でのコードを反映
    ・共通部分(キーボード操作、マウス)のコードを単一化
    ・キーボード操作に対応。
    ・同時押しに対応した為音階の表示を7音分追加。

--- tonepiano.js
+++ tonepiano.js
@@ -2,267 +2,143 @@ $(window).on("load", function () {
   const volume = new Tone.Volume(0);
   const synth = new Tone.PolySynth(Tone.Synth).connect(volume);
   volume.toDestination();
-  const activeKey = {};
   Tone.Transport.bpm.value = 60;
-  let textHideCount = null;
-  let playing = false;
+  let soundTimeoutCount = [];
+  let activeSound = [];
+
   //キー割り当て、鳴らす音、表示用テキスト(自動演奏に対応する為に変更必要)
-  const note = {
-    a: { pitch: "C4", text: "ド" },
-    s: { pitch: "D4", text: "レ" },
-    d: { pitch: "E4", text: "ミ" },
-    f: { pitch: "F4", text: "ファ" },
-    g: { pitch: "G4", text: "ソ" },
-    h: { pitch: "A4", text: "ラ" },
-    j: { pitch: "B4", text: "シ" },
-  };
+  const text = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];
+  const keyBind = { a: 0, s: 1, d: 2, f: 3, g: 4, h: 5, j: 6 };
+  const pitch = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
 
-  //マウス操作
-  let clickMonitor = false;
-  function mouseDown() {
-    clickMonitor = true;
-    console.log(`マウスボタンが${clickMonitor}です。`);
-  }
-
-  function mouseUp() {
-    clickMonitor = false;
-    console.log(clickMonitor);
-    console.log(`マウスボタンが${clickMonitor}です。`);
-  }
-
-  $(document).on("mousedown", mouseDown);
-  $(document).on("mouseup", mouseUp);
-
-  //キーボード操作(押した時)
-  $(document).on("keydown", function pushKey(e) {
-    const key = e.key.toLowerCase();
-    if (note[key] && !activeKey[key] && !playing && !clickMonitor) {
-//      if (Tone.context.state !== "running") {
-        Tone.start();
-  //      console.log("Tone.context.state !== runningのフラグ");
-  //    }
-      synth.triggerAttack(note[key].pitch);
-      activeKey[key] = true;
-
-      clearTimeout(textHideCount);
-      $("#sound_view").text(note[key].text);
-      console.log(note[key].text);
+  //音を再生
+  function playSound(i) {
+    if (!activeSound[i] && !playing) {
+      synth.triggerAttack(pitch[i]);
+      activeSound[i] = true;
+      Tone.start();
+      clearTimeout(soundTimeoutCount[i]);
+      $(".sound_view").eq(i).text(text[i]);
     }
-  });
+  }
 
-  //キーボード操作(離した時)
-  $(document).on("keyup", (e) => {
-    const key = e.key.toLowerCase();
-    if (note[key] && !playing) {
-      synth.triggerRelease(note[key].pitch);
-      activeKey[key] = false;
-      textHideCount = setTimeout(() => {
-        if (Object.values(activeKey).every((value) => value === false)) {
-          $("#sound_view").text("");
-        }
+  //音を停止
+  function stopSound(i) {
+    if (!playing) {
+      synth.triggerRelease(pitch[i]);
+      clearTimeout(soundTimeoutCount[i]);
+      activeSound[i] = false;
+      soundTimeoutCount[i] = setTimeout(function () {
+        $(".sound_view").eq(i).text("");
       }, 1000);
     }
-  });
-
-  //音量管理
-  function volumeToGain(volumeValue) {
-    if (volumeValue === 0) {
-      return -Infinity;
-    } else {
-      var gains = volumeValue * 40 - 40;
-      return gains;
-    }
   }
 
-  $("#volume_control").on("input", function (e) {
-    const volumeValue = parseFloat($(this).val());
-    volume.volume.value = volumeToGain(volumeValue);
+  //マウスを押した時
+  $(".key").on("mousedown", function () {
+    let i = $(".key").index(this);
+    playSound(i);
   });
 
-  //きらきら星の楽譜
-  const starSong = [
-    { note: "C4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "C4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "A4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "A4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "2n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "C4", duration: "2n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "2n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "2n" },
-    { note: "C4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "C4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "A4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "A4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "G4", duration: "2n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "F4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "rest", duration: "8n" },
-    { note: "C4", duration: "1n" },
-  ];
+  //マウスを離した時
+  $(".key").on("mouseup mouseout", function () {
+    let i = $(".key").index(this);
+    stopSound(i);
+  });
 
-  //チューリップの楽譜
-  const tulipSong = [
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "4n" },
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "4n" },
-    { note: "G4", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "D4", duration: "4n" },
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "4n" },
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "4n" },
-    { note: "G4", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "C4", duration: "8n" },
-    { note: "D4", duration: "8n" },
-    { note: "E4", duration: "8n" },
-    { note: "C4", duration: "4n" },
-    { note: "G4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "G4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "E4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "G4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "A4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "A4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "G4", duration: "4n" },
-    { note: "E4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "E4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "D4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "D4", duration: "16n" },
-    { note: "rest", duration: "16n" },
-    { note: "C4", duration: "1n" },
-  ];
-
-  //自動演奏ボタンを押して状態を確認してから分岐
-  $("#playButton").on("mouseup", async function () {
-    if (playing) {
-      stopSong();
-    } else if (
-      Object.values(activeKey).every((value) => value === false) &&
-      !clickMonitor == false
-    ) {
-      await playSong();
+  //キーを押して再生
+  $(document).on("keydown", function (e) {
+    let i = e.key;
+    if (keyBind[i] !== -1 && !e.repeat) {
+      playSound(keyBind[i]);
     }
   });
 
-  //演奏開始
-  async function playSong() {
-    await Tone.start();
-    $("#playButton").text("自動演奏停止");
-    console.log("自動演奏を開始");
-    playing = true;
-    const song = Math.random() < 0.5 ? starSong : tulipSong;
+  //キーを離した時
+  $(document).on("keyup", function (e) {
+    let i = e.key;
+    if (keyBind[e.key] !== -1) {
+      stopSound(keyBind[i]);
+    }
+  });
 
-    Tone.Transport.cancel();
-    let time = 0;
-    for (let i = 0; i < song.length; i++) {
-      const currentNote = song[i];
-      if (currentNote.note !== "rest") {
-        Tone.Transport.scheduleOnce((test) => {
-          synth.triggerAttackRelease(
-            currentNote.note,
-            currentNote.duration,
-            test
-          );
-          //演奏中のテキスト表示が必要
-        }, time);
+  //自動演奏
+  Tone.Transport.bpm.value = 60;
+  let playing = false; //自動演奏が有効化か
+  let autoPlayTimeout = []; //タイムアウト管理
+
+  //曲の管理(別ファイル)
+  const songList = [
+    { name: "きらきら星", note: starSong },
+    { name: "チューリップ", note: tulipSong },
+  ];
+
+  //自動演奏停止の為のフラグ
+  let stopAutoPlay = false;
+
+  function playSong(song) {
+    let currentTime = Tone.now();
+    $("#autoplay_text").text(`${song.name}を演奏中`);
+    let totalDuration = 0;
+    stopAutoPlay = false;
+    for (let i = 0; i < autoPlayTimeout.length; i++) {
+      clearTimeout(autoPlayTimeout[i]);
+    }
+    autoPlayTimeout = []; //リセット
+
+    for (let i = 0; i < song.note.length; i++) {
+      let note = song.note[i];
+      if (note.note !== "rest") {
+        let timeoutCount = setTimeout(function () {
+          if (!stopAutoPlay) {
+            synth.triggerAttackRelease(note.note, note.duration);
+          }
+        }, totalDuration * 1000);
+        autoPlayTimeout.push(timeoutCount);
       }
-      time += Tone.Time(currentNote.duration).toSeconds();
+      totalDuration += Tone.Time(note.duration).toSeconds();
     }
 
-    //演奏が終わったらstopSong実行
-    Tone.Transport.scheduleOnce(() => {
-      stopSong();
-    }, time);
-    Tone.Transport.start();
+    let endTime = setTimeout(function () {
+      if (!stopAutoPlay) {
+        playing = false;
+        $("#autoplay_text").text("");
+        $("#playButton").text("自動演奏");
+      }
+    }, totalDuration * 1000);
+    autoPlayTimeout.push(endTime);
   }
-  //演奏停止
-  function stopSong() {
-    $("#playButton").text("自動演奏開始");
-    Tone.Transport.cancel();
-    Tone.Transport.stop();
-    playing = false;
-    console.log("自動演奏停止しました。");
-    $("#sound_view").text("");
-  }
+
+  $("#playButton").on("click", function () {
+    if (playing) {
+      stopAutoPlay = true;
+      playing = false;
+      synth.releaseAll();
+      autoPlayTimeout = []; //リセット
+      $(this).text("自動演奏");
+      $("#autoplay_text").text("");
+      for (let i = 0; i < autoPlayTimeout.length; i++) {
+        clearTimeout(autoPlayTimeout[i]);
+      }
+    } else {
+      playing = true;
+      stopAutoPlay = false;
+      for (let i = 0; i < pitch.length; i++) {
+        $(".sound_view").eq(i).text("");
+      }
+      $(this).text("演奏停止");
+      const i = Math.floor(Math.random() * songList.length);
+      playSong(songList[i]);
+    }
+  });
+
+  //追加機能
+  $("#volume_control").on("input change", function () {
+    const volumeValue = parseFloat($(this).val());
+    if (volumeValue === 0) {
+      volume.volume.value = -Infinity;
+    } else {
+      volume.volume.value = volumeValue * 40 - 40;
+    }
+  });
 });
