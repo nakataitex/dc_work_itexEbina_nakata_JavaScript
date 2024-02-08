6b19d8e6291bbef629ac55d9f563783e1aa65822
nakataitex <nakatahirokiitex@gmail.com>
2024-02-08 13:50:28 +0900

    不具合修正
    ・自動演奏が中断されて、再度自動演奏を押した時に前回の続きの音も一緒に再生されていた不具合を修正。
    ・使用していなかった変数を削除。
    ・jsファイルの名前をmainに変更。

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(window).on("load", function () {
   let soundTimeoutCount = [];
   let activeSound = [];
 
-  //キー割り当て、鳴らす音、表示用テキスト(自動演奏に対応する為に変更必要)
+  //表示用テキスト、キー割り当て、鳴らす音
   const text = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"];
   const keyBind = { a: 0, s: 1, d: 2, f: 3, g: 4, h: 5, j: 6 };
   const pitch = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
@@ -23,7 +23,6 @@ $(window).on("load", function () {
     }
   }
 
-  
   //音を停止
   function stopSound(i) {
     if (!playing) {
@@ -36,8 +35,6 @@ $(window).on("load", function () {
     }
   }
 
-
-  
   //マウスを押した時
   $(".key").on("mousedown", function () {
     let i = $(".key").index(this);
@@ -67,9 +64,9 @@ $(window).on("load", function () {
   });
 
   //自動演奏
-  Tone.Transport.bpm.value = 60;
   let playing = false; //自動演奏が有効化か
-  let autoPlayTimeout = []; //タイムアウト管理
+  let autoPlayTimeout = [];
+  let stopAutoPlay = false;
 
   //曲の管理(別ファイル)
   const songList = [
@@ -77,19 +74,15 @@ $(window).on("load", function () {
     { name: "チューリップ", note: tulipSong },
   ];
 
-  //自動演奏停止の為のフラグ
-  let stopAutoPlay = false;
-
+  //自動演奏
   function playSong(song) {
-    let currentTime = Tone.now();
     $("#autoplay_text").text(`${song.name}を演奏中`);
     let totalDuration = 0;
     stopAutoPlay = false;
     for (let i = 0; i < autoPlayTimeout.length; i++) {
       clearTimeout(autoPlayTimeout[i]);
     }
-    autoPlayTimeout = []; //リセット
-
+    autoPlayTimeout = [];
     for (let i = 0; i < song.note.length; i++) {
       let note = song.note[i];
       if (note.note !== "rest") {
@@ -113,30 +106,31 @@ $(window).on("load", function () {
     autoPlayTimeout.push(endTime);
   }
 
+  //ボタンを押した時(自動演奏中か判断して分岐)
   $("#playButton").on("click", function () {
     if (playing) {
+      console.log("演奏停止しました");
       stopAutoPlay = true;
       playing = false;
       synth.releaseAll();
-      autoPlayTimeout = []; //リセット
       $(this).text("自動演奏");
       $("#autoplay_text").text("");
-      for (let i = 0; i < autoPlayTimeout.length; i++) {
-        clearTimeout(autoPlayTimeout[i]);
-      }
     } else {
       playing = true;
       stopAutoPlay = false;
+
       for (let i = 0; i < pitch.length; i++) {
         $(".sound_view").eq(i).text("");
       }
       $(this).text("演奏停止");
       const i = Math.floor(Math.random() * songList.length);
       playSong(songList[i]);
+      console.log("演奏開始しました");
     }
   });
 
   //追加機能
+  //音量調整
   $("#volume_control").on("input change", function () {
     const volumeValue = parseFloat($(this).val());
     if (volumeValue === 0) {
